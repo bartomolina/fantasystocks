6bf8082a887ceca6ad00552e506744e0ee9b171c
Barto Molina <bartomolina@gmail.com>
2018-01-01 12:15:11 -0500

    fixed login

--- client/src/redux/mappers/dispatchers.js
+++ client/src/redux/mappers/dispatchers.js
@@ -3,7 +3,6 @@ import * as store from '../../redux'
 export function loginDispatcher(dispatch) {
   return {
     getUser: () => {
-      console.log('getting user')
       dispatch(store.getUser())
     }
   }
